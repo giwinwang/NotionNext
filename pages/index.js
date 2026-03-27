65889324ac5880c3914252679a26bb5a5cc166e3
giwinwang <giwinwang@gmail.com>
2026-03-27 08:39:43 +0800

    Refactor index.js to use React for redirection
    Removed HTML structure and added React component to redirect to Notion.

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,20 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <title>Giwin</title>
-</head>
-<body>
-  <script>
+import { useEffect } from 'react';
+
+export default function Home() {
+  useEffect(() => {
+    // 直接跳转到你的 Notion，地址栏保持 giwin.cn
     window.location.replace("https://giwin.notion.site");
-  </script>
-</body>
-</html>
+  }, []);
+  
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      fontSize: '18px'
+    }}>
+      正在打开 Giwin Space...
+    </div>
+  );
+}
